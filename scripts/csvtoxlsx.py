0816956e6c955f64f63bc1cc1a3ecd7725f66466
Xtormin <jtorresf42@gmail.com>
2022-11-30 22:16:57 +0100

    New script csvtoxlsx

---
+++ scripts/csvtoxlsx.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import argparse
+
+parser = argparse.ArgumentParser(add_help = True, description = '%(prog)s hunts all forms and inputs found in a list of urls.')
+
+parser.add_argument('-c','--csvfile', 
+                    help = 'XLSX output of CSV file', 
+                    nargs = '?', type=str)
+
+if __name__=="__main__":
+    args = parser.parse_args()
+
+    filename = args.csvfile
+    filename_xlsx = f"{filename}.xlsx"
+
+    try:
+        df = pd.read_csv(filename, sep=";")
+        df.reset_index(drop=True)
+        df.style.to_excel(filename_xlsx, index=False)
+        print(f"|+| File created on {filename_xlsx}")
+    except Exception as e:
+        print(f"|+| File not created")
+        print(e)
